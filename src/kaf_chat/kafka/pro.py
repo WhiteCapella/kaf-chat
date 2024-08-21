6ea3954b04653a3a003d92485afa5993697f38f4
WhiteCapella <whitecapella.official@gmail.com>
2024-08-21 12:10:35 +0900

    send_message

--- src/kaf_chat/kafka/pro.py
+++ src/kaf_chat/kafka/pro.py
@@ -1,22 +1,22 @@
 from kafka import KafkaProducer
 
-import keyboard
 import time
 import json
-
-producer = KafkaProducer (
+def send_message():
+    producer = KafkaProducer (
         bootstrap_servers=['localhost:9092'],
         value_serializer=lambda x:json.dumps(x).encode('utf-8')
         )
 
-start = time.time()
-while(True):
-    if keyboard.is_pressed('f4'):  # F4 키가 눌렸는지 확인
-        break
-    message = input(">>")  # 사용자 입력 받기
-    producer.send('topic1', value={'message': message})  # Kafka 토픽에 전송
-    producer.flush()  # 메시지 전송 완료
+    start = time.time()
+    while(True):
+        #TODO F4키가 눌려질 경우 종료해버리기
+        # if문을 통해서 시도해보았으나 keyboard 모듈 미인식 문제로 대기
+
+        message = input(">> ")  # 사용자 입력 받기
+        producer.send('topic1', value={"topic1": message})  # Kafka 토픽에 전송
+        producer.flush()  # 메시지 전송 완료
 
 
-end = time.time()
-print("[DONE}:", end - start)
+    end = time.time()
+    print("[DONE}:", end - start)
