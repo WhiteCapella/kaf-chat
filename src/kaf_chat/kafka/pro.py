0745ed7aff30870210c0c119c87675b9a6b865aa
WhiteCapella <whitecapella.official@gmail.com>
2024-08-21 17:13:16 +0900

    test

--- src/kaf_chat/kafka/pro.py
+++ src/kaf_chat/kafka/pro.py
@@ -4,23 +4,22 @@ import time
 import json
 def send_message():
     group_id = input("Enter Chatroom : ")
-    topic = input("Enter Nickname : ")
-    producer = KafkaProducer (
-        topic = topic,
+    topic = f"chatroom_{group_id}"  # group_id에 따라 토픽 생성
+    nickname = input("Enter Nickname : ")
+
+    producer = KafkaProducer(
         bootstrap_servers=['localhost:9092'],
-        group_id=group_id,
-        value_serializer=lambda x:json.dumps(x).encode('utf-8'),
-        )
+        value_serializer=lambda x: json.dumps(x).encode('utf-8'),
+    )
 
     start = time.time()
-    while(True):
-        message = input(">> ")  # 사용자 입력 받기
+    while True:
+        message = input(">> ")
         if message == "exit":
             break
-        m_message = f'{topic} >> {message}'
-        producer.send(topic, value=m_message)  # Kafka 토픽에 전송
-        producer.flush()  # 메시지 전송 완료
-
+        m_message = f'{nickname} >> {message}'
+        producer.send(topic, value=m_message) 
+        producer.flush() 
 
 
     end = time.time()
