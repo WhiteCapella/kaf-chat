9e915833859496cd892324794869b8e0bc9438ce
WhiteCapella <elnath1002@naver.com>
2024-08-26 06:04:27 +0900

    fix/final/2

--- src/kaf_chat/kafka/chat.py
+++ src/kaf_chat/kafka/chat.py
@@ -1,61 +1,68 @@
 from kafka import KafkaConsumer, KafkaProducer
-from json import loads
 
+import json
 import time
 import threading
 
 is_producer = False
-server_add="172.17.0.1:9092"
+server_add="localhost:9092"
 
-def mode_receive(partner_topic):
-    global is_producer
-    global server_add
+def mode_receive(chatroom):
+	global is_producer
+	global server_add
 
-    consumer = KafkaConsumer(
-            bootstrap_servers=server_add,
-        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-    )
-    consumer.subscribe([partner_topic])
+	consumer = KafkaConsumer(
+		bootstrap_servers=server_add,
+		auto_offset_reset='latest',
+		enable_auto_commit=True,
+		value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+	)
+	consumer.subscribe([chatroom])
 
-    for message in consumer:
-        print(f"Received message: {message.value}")
-        if is_producer:  # Producer 모드로 전환되었으면 Consumer 종료
-            break
+	for message in consumer:
+		data = message.value
+		print(f"{data['nickname']} >> {data['message']} : {data['time']}")
+		if is_producer:  # Producer 모드로 전환되었으면 Consumer 종료
+			break
 
-def mode_send(my_topic):
-    global is_producer
-    global server_add
+
+def mode_send(chatroom, nickname):
+	global is_producer
+	global server_add
     
-    producer = KafkaProducer(
-        bootstrap_servers=server_add,
-        value_serializer=lambda x:json.dumps(x).encode('utf-8'),
-    )
-    while is_producer:
-        message = input(">> ")
-        producer.send(my_topic, value=message)
-        producer.flush()
+	producer = KafkaProducer(
+		bootstrap_servers=server_add,
+		value_serializer=lambda x:json.dumps(x).encode('utf-8'),
+	)
+	while is_producer:
+		message = input(">> ")
+		if message == 'exit':
+			producer.close()
+		data = {'nickname':nickname, 'message':message, 'time':time.time()}
+		producer.send(chatroom, value=data)
+		producer.flush()
 
 
 def input_check():
-    global is_producer
+	global is_producer
 
-    while True:
-        input()  # Enter 키 입력 대기
-        is_producer = not is_producer
+	while True:
+		input()  # Enter 키 입력 대기
+		is_producer = not is_producer
 
 def make_chat():
-    arg1 = input("Enter Your Nickname : ")
-    arg2 = input("Enter Partner Nickname : ")
+	nickname = input("Enter Your Nickname : ")
+	chatroom = input("Enter Chatroom Name : ")
 
-    consumer_thread = threading.Thread(target=mode_receive, args=(arg2,))
-    producer_thread = threading.Thread(target=mode_send, args=(arg1,))
-    input_thread = threading.Thread(target=input_check)
+	consumer_thread = threading.Thread(target=mode_receive, args=(chatroom,))
+	producer_thread = threading.Thread(target=mode_send, args=(chatroom, nickname,))
+	input_thread = threading.Thread(target=input_check)
 
-    consumer_thread.start()
-    producer_thread.start()
-    input_thread.start()
+	consumer_thread.start()
+	producer_thread.start()
+	input_thread.start()
 
-    consumer_thread.join()
-    producer_thread.join()
-    input_thread.join()
+	consumer_thread.join()
+	producer_thread.join()
+	input_thread.join()
 
